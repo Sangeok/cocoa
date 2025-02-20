6fafe5b030957cb1611353f7162e34645d041c77
joshephan <joshephan0204@gmail.com>
2025-02-20 19:42:37 +0900

    fix: 광고 배너 늘어짐 조정

--- client/components/event/EventBanner.tsx
+++ client/components/event/EventBanner.tsx
@@ -3,14 +3,14 @@ import Image from "next/image";
 
 export default function EventBanner() {
   return (
-    <section className="lg:w-1/3 w-full lg:m-6">
+    <section className="lg:w-1/3 w-full flex flex-col">
       <Link href="/event">
         <Image
           src="/images/event-banner.png"
           alt="event banner"
           width={400}
           height={400}
-          className="w-full h-full object-cover"
+          className="w-full h-full object-contain"
         />
       </Link>
     </section>
