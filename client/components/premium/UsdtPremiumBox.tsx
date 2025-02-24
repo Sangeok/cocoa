9d8fa9a5c187429784c8177e9666596f211ccacd
joshephan <joshephan0204@gmail.com>
2025-02-24 21:36:49 +0900

    fix: 모바일 테더 간격 조정

--- client/components/premium/UsdtPremiumBox.tsx
+++ client/components/premium/UsdtPremiumBox.tsx
@@ -13,8 +13,8 @@ export function UsdtPremiumBox() {
 
   return (
     <div className="bg-white dark:bg-gray-950 rounded-lg p-4 sm:p-6 border border-gray-200 dark:border-gray-900">
-      <div className="flex flex-wrap items-center gap-2 lg:gap-4">
-        <div className="flex items-center gap-6">
+      <div className="flex flex-wrap items-center gap-1 sm:gap-2 lg:gap-4">
+        <div className="flex items-center gap-2 sm:gap-4 lg:gap-6">
           <div className="flex items-center gap-2">
             <Image
               src={`${UPBIT_STATIC_IMAGE_URL}/USDT.png`}
@@ -39,7 +39,7 @@ export function UsdtPremiumBox() {
               />
               업비트
             </div>
-            <div className="text-sm font-medium text-right">
+            <div className="text-sm font-medium">
               ₩{formatNumber(usdtUpbit)}
             </div>
           </div>
@@ -53,16 +53,18 @@ export function UsdtPremiumBox() {
               />
               빗썸
             </div>
-            <div className="text-sm font-medium text-right">
+            <div className="text-sm font-medium">
               ₩{formatNumber(usdtBithumb)}
             </div>
           </div>
           <div className="text-sm text-gray-500 dark:text-gray-400">
             <div className="text-sm text-gray-500 dark:text-gray-400">환율</div>
-            <div className="text-sm font-medium">₩{formatNumber(Math.round(rate))}</div>
+            <div className="text-sm font-medium">
+              ₩{formatNumber(Math.round(rate))}
+            </div>
           </div>
         </div>
-        <div className="flex items-center gap-6">
+        <div className="flex items-center gap-2 sm:gap-4 lg:gap-6">
           <div>
             <div className="text-sm text-gray-500 dark:text-gray-400">
               프리미엄
